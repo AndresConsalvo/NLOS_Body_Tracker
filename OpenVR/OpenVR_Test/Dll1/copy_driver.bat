0da5431422bde333990c07c43401c3a214dca039
Ruo Chen <chenruo@ufl.edu>
2022-02-20 10:28:26 -0500

    Added batch script to automatically copy files to driver folder

---
+++ OpenVR/OpenVR_Test/Dll1/copy_driver.bat
@@ -0,0 +1,1 @@
+xcopy /y "C:\Users\chenr\Desktop\NLOS\NLOS_Body_Tracker\OpenVR\OpenVR_Test\Dll1\x64\Release\driver_NLOS.dll" "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\drivers\NLOS\bin\win64"
