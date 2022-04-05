cf8afaff31952f6e187059da1361275393fcd3b9
Ruo Chen <chenruo@ufl.edu>
2022-04-05 12:27:37 -0400

    rearranged joe's code

--- Hardware/PythonUART/uart_send.py
+++ Hardware/PythonUART/uart_send.py
@@ -1,33 +1,47 @@
 import serial
+import socket
 
 
-def setNetwork(port, ssid, password):
+def setNetwork(port, ssid, password, ip_bytes, net_port):
 
     print("Setting network credentials")
-    send_string = ssid + '\n' + password + '\n'
-    send_string = send_string.encode()
+    send_str = ssid.encode() + b'\x0A' + password.encode() + b'\x0A' + ip_bytes + b'\x0A' + net_port + b'\x0A'
+    print(send_str)
 
-    port.write(send_string)
+    port.write(send_str)
 
 
     s = port.readline()
     print("SSID set as: ", s)
     p = port.readline()
     print("Password set as: ", p)
+    ip_ret = port.readline()
+    print(len(ip_ret))
+    print("IP set as: ", ip_ret)
+    port_ret = port.readline()
+    print(len(port_ret))
+    print("Port set as: ", port_ret)
+
 
 # to do: if no write back with correct response, then assume that the ssid and password did not get sent, and resend it.
 
 print("Hi!")
 
-ser = serial.Serial('COM4', 115200, timeout = 1)
-ser.is_open
+ser = serial.Serial("COM4", 115200, timeout = 1)
 
 ssid = input("Enter SSID: ")
 password = input("Enter password: ")
+ip = input("Enter IP: ")
+ip_bytes = socket.inet_aton(ip)
 
-setNetwork(ser, ssid, password)
+port = input("Enter port: ")
+port = int(port)
+port = port.to_bytes(2, 'big')
+
+setNetwork(ser, ssid, password, ip_bytes, port)
+#ser.close()
 
 # reads info from the mcu
+count = 0
 while 1:
-    ret = ser.readline()
-    print(ret)
+    count = count + 1
--- Hardware/the_amalgamation/the_amalgamation.ino
+++ Hardware/the_amalgamation/the_amalgamation.ino
@@ -1,18 +1,68 @@
 #include <Arduino.h>
+#include "EEPROM.h"
 #include "wifi_connect.h"
 #include "io.h"
 #include "udp.h"
 #include "imu.h"
 
+int serialLength;
+char ssid[128];
+char password[128];
+char ip[5];
+char port[16];
+
+byte ip_arr[4];
+short net_port;
+
+int send_length;
+
+int receive_length_ssid;
+int receive_length_pass;
+int receive_length_ip;
+int receive_length_port;
 
 void setup() {
   Serial.begin(115200);
+
+  if (!EEPROM.begin(EEPROM_SIZE)) {
+      Serial.println("failed to init EEPROM");
+      delay(1000000);
+  }
+  
+  ip[0] = EEPROM.read(0);
+  ip[1] = EEPROM.read(1);
+  ip[2] = EEPROM.read(2);
+  ip[3] = EEPROM.read(3);
+  net_port = EEPROM.read(4) << 8 | EEPROM.read(5);
+    
   io_init();
   wifiConnect();
   imu_init();
 }
 
 void loop() {
+
+  serialLength = Serial.available();
+  
+  if (serialLength) {
+    receive_length_ssid = Serial.readBytesUntil('\n', ssid, sizeof(ssid));
+    receive_length_pass = Serial.readBytesUntil('\n', password, sizeof(password));
+    receive_length_ip   = Serial.readBytesUntil('\n', ip, sizeof(ip));
+    receive_length_port = Serial.readBytesUntil('\n', port, sizeof(port));
+
+    //wifiConnect(ssid, password);
+    
+    ssid[receive_length_ssid] = '\n';
+    password[receive_length_pass] = '\n';
+    
+    send_length = Serial.write(ssid);
+    Serial.write(password);
+    
+    memset(ssid, 0, sizeof(ssid));
+    memset(password, 0, sizeof(password));
+    memset(ip, 0, sizeof(ip));
+  }
+  
   printImuValues();
   data_send();  
   delay(10);
