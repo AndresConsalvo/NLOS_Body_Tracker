bfd0031852337cd3e64a566742c557390846eb3f
Ruo Chen <chenruo@ufl.edu>
2022-04-06 17:25:10 -0400

    Update uart_send.py

--- Hardware/PythonUART/uart_send.py
+++ Hardware/PythonUART/uart_send.py
@@ -2,9 +2,9 @@ import serial
 import socket
 
 
-def setNetwork(port, ssid, password, ip_bytes, tracker_id):
+def setNetwork(ser, ssid, password, ip_bytes, tracker_id):
     try:
-        ser = serial.Serial("COM4", 115200, timeout = 1)
+       
         print("Setting network credentials")
         send_str = ssid.encode() + b'\x0A' + password.encode() + b'\x0A' + ip_bytes + b'\x0A' + tracker_id + b'\x0A'
         print(send_str)
@@ -30,7 +30,7 @@ def setNetwork(port, ssid, password, ip_bytes, tracker_id):
 # to do: if no write back with correct response, then assume that the ssid and password did not get sent, and resend it.
 
 print("Hi!")
-
+ser = serial.Serial("COM4", 115200, timeout = 1)
 COM = input("Enter COM port: ")
 
 ssid = input("Enter SSID: ")
@@ -48,5 +48,5 @@ id = input("Enter id: ")
 id = int(id)
 id = id.to_bytes(1, 'big')
 
-setNetwork(COM, ssid, password, ip_bytes, id)
+setNetwork(ser, ssid, password, ip_bytes, id)
 
